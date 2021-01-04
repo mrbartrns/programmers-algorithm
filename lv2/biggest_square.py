ed336764a2026a4295ba186e0ea1e8d34299f495
Junhyuck <mrbartrns@naver.com>
2021-01-04 09:32:47 +0900

    not working

---
+++ lv2/biggest_square.py
@@ -0,0 +1,8 @@
+def solution(board):
+    answer = 0
+    # row_start_point = 0
+    # row_end_point = len(board)
+    # col_start_point = 0
+    # col_end_point = len(board[0])
+
+    return answer
